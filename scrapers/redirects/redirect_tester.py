cd44228a8fdfe70f158904df508433d963b9d7c6
Marquis <theusual@icloud.com>
2019-09-17 12:20:13 -0600

    procfile

--- scrapers/redirects/redirect_tester.py
+++ scrapers/redirects/redirect_tester.py
@@ -1,11 +1,15 @@
 from urllib.request import urlopen, Request, HTTPError
 from time import sleep
 import csv
+import ssl
 
 # set up csv read
 with open('redirect_urls.csv', 'r') as csv_file:
+    # SSL bypass
+  ssl._create_default_https_context = ssl._create_unverified_context
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
   csv_reader = csv.reader(csv_file, delimiter=',')
+  target_index = 4
 
   # set up csv write
   with open('checked.csv', 'w', newline='') as output_target:
@@ -13,7 +17,7 @@ with open('redirect_urls.csv', 'r') as csv_file:
 
     for row in csv_reader:
       # swap url
-      url = row[0]
+      url = row[target_index]
 
       # send request
       try:
@@ -29,4 +33,3 @@ with open('redirect_urls.csv', 'r') as csv_file:
       write_csv.writerow([f'{url}', f'{status}'])
 
       # sleep(2)
-
